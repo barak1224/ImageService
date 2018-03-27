d560ec3f74f0db287c55f74fc992c9650ca612d8
Iosi Ginerman <iginerman@hotmail.com>
2018-03-27 12:58:59 +0300

    Allow for month names

--- ImageService/ImageService/Model/ImageServiceModel.cs
+++ ImageService/ImageService/Model/ImageServiceModel.cs
@@ -35,7 +35,7 @@ namespace ImageService.Model
             // get the time of creation of the file
             DateTime date = File.GetCreationTime(path);
             string yearFolder = Path.Combine(OutputFolder, date.Year.ToString());
-            string monthFolder = Path.Combine(yearFolder, date.Month.ToString());
+            string monthFolder = Path.Combine(yearFolder, GetMonth(date.Month));
             string destFile = Path.Combine(monthFolder, Path.GetFileName(path));
 
             //creates the folder. If it exists, it does nothing.
@@ -68,5 +68,24 @@ namespace ImageService.Model
             throw new NotImplementedException();
         }
 
+        private static string GetMonth(int monthNumber)
+        {
+            switch (monthNumber)
+            {
+                case 1: return "January";
+                case 2: return "February";
+                case 3: return "March";
+                case 4: return "April";
+                case 5: return "May";
+                case 6: return "June";
+                case 7: return "July";
+                case 8: return "August";
+                case 9: return "September";
+                case 10: return "October";
+                case 11: return "November";
+                case 12: return "December";
+                default: return "Default";
+            }
+        }
     }
 }
