68524b5b9ba60dbc210481d2c6855d8044cfe5f1
Barak <barak1224@gmail.com>
2018-05-02 13:55:13 +0200

    Updates view in the folders

--- UI/View/LogTab.xaml.cs
+++ UI/View/LogTab.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace UI
+namespace UI.View
 {
     /// <summary>
     /// Interaction logic for LogTab.xaml
