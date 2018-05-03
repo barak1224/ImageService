f49c682e8543dcba5e76b1db62fe0349b568e46c
Iosi Ginerman <iginerman@hotmail.com>
2018-05-03 13:37:08 +0300

    Saving logtab

--- UI/View/LogTab.xaml.cs
+++ UI/View/LogTab.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UI.ViewModel;
 
 namespace UI.View
 {
@@ -23,6 +24,7 @@ namespace UI.View
         public LogTab()
         {
             InitializeComponent();
+            logList.ItemsSource = new LogViewModel().LogEntries;
         }
     }
 }
---
+++ UI/ViewModel/LogViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UI.ViewModel
+{
+    public class LogViewModel
+    {
+        public LogEntryList LogEntries { get; set; }
+
+        public LogViewModel()
+        {
+            LogEntries = new LogEntryList { new LogEntry(LType.INFO, "This in info"),
+                new LogEntry(LType.ERROR, "This in error"),
+                new LogEntry(LType.WARNING, "This in warning") };
+        }
+    }
+
+    public class LogEntryList : ObservableCollection<LogEntry> { }
+
+    public class LogEntry
+    {
+        public LogEntry(LType logType, string message)
+        {
+            LogType = logType;
+            Message = message;
+        }
+
+        public LType LogType { get; }
+        public String Message { get; }
+
+        public String GetLogTypeAsString(LType type)
+        {
+            if (type == LType.INFO)
+                return "INFO";
+            else if (type == LType.ERROR)
+                return "ERROR";
+            else return "WARNING";
+        }
+    }
+
+    public enum LType
+    {
+        INFO,
+        ERROR,
+        WARNING,
+    }
+}
