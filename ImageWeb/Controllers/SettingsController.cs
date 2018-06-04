50f2f82967ee842095f8f0dce495deaf9d5991de
Barak <barak1224@gmail.com>
2018-06-04 18:47:18 +0200

    update bootstrap

---
+++ ImageWeb/Controllers/SettingsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageWeb.Controllers
+{
+    public class SettingsController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "This is settings.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
